0ccef82bc1e941177102853aeb31b47495a121dc
pascal <p.jipp87@gmail.com>
2021-12-28 12:18:13 +0100

    .

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MainScreen } from "./src/Screens/MainScreen";
+import { SettingsScreen } from "./src/Screens/SettingsScreen";
+import { FriendsScreen } from "./src/Screens/FriendsScreen";
+import { MessageScreen } from "./src/Screens/MessageScreen";
+import { NewsScreen } from "./src/Screens/NewsScreen";
+import { useState } from "react";
+import React from "react";
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from "@react-navigation/native";
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import merge from "deepmerge";
+import { PreferencesContext } from "./src/utils/ThemeContext";
+
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [isThemeDark, setIsThemeDark] = React.useState(false);
+
+  let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark]
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PreferencesContext.Provider value={preferences}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>
+          <Tab.Navigator>
+            <Tab.Screen name="Startseite" component={MainScreen} />
+            <Tab.Screen name="Neuigkeiten" component={NewsScreen} />
+            <Tab.Screen name="Freunde" component={FriendsScreen} />
+            <Tab.Screen name="Nachrichten" component={MessageScreen} />
+            <Tab.Screen name="Einstellungen" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </PreferencesContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
