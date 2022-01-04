1eea380696c23e987b8b0606b62663cf4ac6bcac
ScarfaceHBC <86946929+ScarfaceHBC@users.noreply.github.com>
2022-01-04 18:09:03 +0100

    ..

--- src/components/FriendInfoModal.js
+++ src/components/FriendInfoModal.js
@@ -4,6 +4,12 @@ import { PreferencesContext } from "../utils/ThemeContext";
 import { Button, Modal } from "react-native-paper";
 import { Vibration } from "react-native";
 import { useTheme } from "react-native-paper";
+import { Image } from "react-native";
+import { useWindowDimensions } from "react-native";
+import { FAB } from "react-native-paper";
+import { useState } from "react";
+import { Portal } from "react-native-paper";
+import { useIsFocused } from "@react-navigation/native";
 
 export const FriendInfoModal = ({ scene }) => {
   const {
@@ -46,24 +52,111 @@ export const FriendInfoModal = ({ scene }) => {
     //setAktiveFriendFunc(null);
     toggleInfoModal();
   };
+
+  const [state, setState] = React.useState({ open: false });
+
+  const onStateChange = ({ open }) => setState({ open });
+  const { open } = state;
+  const isFocused = useIsFocused();
+
   return (
     <Modal
       visible={true}
-      contentContainerStyle={{ flex: 0.8, backgroundColor: colors.background }}
+      contentContainerStyle={{ flex: 1, backgroundColor: colors.background }}
     >
       <View style={{ flex: 1, paddingTop: 25 }}>
-        <Text style={{ color: colors.text, textAlign: "center" }}>
+        <Image
+          source={{ uri: aktiveFriend.picture.large }}
+          style={{
+            width: useWindowDimensions().width * 0.5,
+            height: useWindowDimensions().width * 0.5,
+            alignSelf: "center",
+            marginBottom: 25,
+            borderWidth: 1.5,
+            borderColor: "black",
+            borderRadius: 15,
+          }}
+        />
+        <Text
+          style={{
+            color: colors.text,
+            textAlign: "center",
+            fontSize: 26,
+            fontWeight: "bold",
+          }}
+        >
           {aktiveFriend.name.first} {aktiveFriend.name.last}
         </Text>
+        <Text
+          style={{
+            color: colors.text,
+            textAlign: "center",
+            fontSize: 16,
+            fontStyle: "italic",
+            paddingVertical: 20,
+          }}
+        >
+          {aktiveFriend.email}
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            paddingHorizontal: 20,
+          }}
+        >
+          <Text style={{ paddingRight: 20 }}>Status:</Text>
+          <Text style={{ width: useWindowDimensions().width * 0.7 }}>
+            {aktiveFriend.text}
+          </Text>
+        </View>
       </View>
 
       <View
         style={{
           flexDirection: "row-reverse",
-          justifyContent: "space-evenly",
+          justifyContent: "center",
           marginBottom: 25,
         }}
       >
+        <View>
+          <Portal>
+            <FAB.Group
+              visible={isFocused}
+              open={open}
+              icon={open ? "minus" : "plus"}
+              actions={[
+                //{ icon: "plus", onPress: () => console.log("Pressed add") },
+                {
+                  icon: "account-remove-outline",
+                  label: "Freund entfernen",
+                  onPress: () => _openAlert(),
+                  color: colors.text,
+                  style: { backgroundColor: colors.error },
+                  small: false,
+                  labelTextColor: colors.text,
+                  labelStyle: colors.background,
+                },
+                // {icon: "email",label: "Email",onPress: () => console.log("Pressed email"),},
+                {
+                  icon: "email",
+                  label: "Nachricht senden",
+                  onPress: () => alert("Funtkcion noch nicht implementiert"),
+                  small: false,
+                  style: { backgroundColor: colors.accent },
+                  color: colors.text,
+                  labelTextColor: colors.text,
+                  labelStyle: colors.disabled,
+                },
+              ]}
+              onStateChange={onStateChange}
+              onPress={() => {
+                if (open) {
+                }
+              }}
+              style={{ paddingVertical: 50 }}
+            />
+          </Portal>
+        </View>
         <Button
           mode="contained"
           raised
@@ -72,18 +165,32 @@ export const FriendInfoModal = ({ scene }) => {
         >
           Zurück
         </Button>
-        <Button
-          compact={true}
-          mode="contained"
-          raised
-          style={{ backgroundColor: colors.notification }}
-          onPress={_openAlert}
-        >
-          Freund Entfernen
-        </Button>
       </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({});
+
+/**
+ *  <FAB
+            style={{ position: "absolute", bottom: 100 }}
+            small
+            visible={fabVisible}
+            icon="plus"
+            onPress={() => console.log("")}
+          />
+          <FAB
+            style={{ position: "absolute", bottom: 50 }}
+            visible={fabVisible}
+            label="Nachricht senden"
+            icon="send"
+            onPress={() => console.log("Message")}
+          />
+          <FAB
+            style={{ position: "relative" }}
+            small
+            icon={fabSymbol}
+            onPress={() => _openFab()}
+          />
+ */
